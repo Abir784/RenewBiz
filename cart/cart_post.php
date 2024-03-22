85738bd4e52c78753b3fb4a9baf6e372cf66be0a
Abir784 <md.abir.hossain2@g.bracu.ac.bd>
2024-03-22 17:11:59 +0600

    merged

--- cart/cart_post.php
+++ cart/cart_post.php
@@ -5,15 +5,14 @@ $id = $_GET['id'];
 $user_id=$_SESSION['login_user_id'];
 $cart_select_query="SELECT * FROM buyer WHERE user_id='$user_id'";
 $buyer_cart_select_query_result=mysqli_query($dbconnect,$cart_select_query);
-$buyer_cart=mysqli_fetch_assoc($cart_select_query_result);
+$buyer_cart=mysqli_fetch_assoc($buyer_cart_select_query_result);
 $bid=$buyer_cart['id'];
-$quantity=$_POST['quantity'];
+$quantity=1;
 date_default_timezone_set('Asia/Dhaka');
 $created_at = date("d-m-y h:i:s");
-
 $query = "INSERT INTO carts(product_id,quantity,buyer_id,created_at) VALUES('$id','$quantity','$bid''$created_at')";
 $insert_query_result = mysqli_query($dbconnect,$query);
 $_SESSION['success']='Added Successfully';
-header('location:add_to_cart.php');
+header('location:../add_to_cart.php?id='.$id);
 
 ?>
