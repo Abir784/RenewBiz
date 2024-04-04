c78e298b06b02ed23bef83d460b766bf1f36e684
Abir784 <md.abir.hossain2@g.bracu.ac.bd>
2024-04-05 01:57:17 +0600

    Order Reciept download, Buyer Dashboard Complete

--- order/order_status_change.php
+++ order/order_status_change.php
@@ -3,6 +3,12 @@
 include '../session_check.php';
 include '../db.php';
 $order_id=$_POST['order_id'];
+$select_order_query="SELECT product_id,weight FROM orders WHERE id='$order_id'";
+$select_order_query_result=mysqli_query($dbconnect,$select_order_query);
+$order=mysqli_fetch_assoc($select_order_query_result);
+$weight=$order['weight'];
+$product_id=$order['product_id'];
+
 date_default_timezone_set('Asia/Dhaka');
 $updated_at = date("d-m-y h:i:s");
 if(isset($_POST['accept'])){
@@ -18,6 +24,8 @@ if(isset($_POST['accept'])){
 }else{
     $update_status= "UPDATE orders SET status=2, updated_at='$updated_at' WHERE id='$order_id'";
     $update_status_result= mysqli_query($dbconnect,$update_status);
+    $update_inventory_query="UPDATE inventory set weight=weight-$weight WHERE product_id='$product_id'";
+    $update_inventory_query_result=mysqli_query($dbconnect,$update_inventory_query);
     $_SESSION['success']="Order Delivered";
 
 }
